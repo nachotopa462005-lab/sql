5362c7c56f2e56cbcc65f39604211bff24d2a6bd
Nicolas ignacio topa <nachotopa462005@gmail.com>
2026-05-05 17:38:29 +0200

    consulta orm

--- mi-app/app/api/products/route.ts
+++ mi-app/app/api/products/route.ts
@@ -1,35 +1,8 @@
-import { sql } from '@/lib/db';
+import { db } from '@/lib/drizzle';
+import { products } from '@/lib/schema';
 
-// Maneja peticiones GET: http://localhost:3000/api/products
 export async function GET() {
-  try {
-    const data = await sql`
-      SELECT 
-        p.name AS producto,
-        p.price AS precio,
-        c.name AS categoria
-      FROM products p
-      INNER JOIN categories c 
-        ON p.category_id = c.id
-    `;
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error: 'Error al obtener productos' }, { status: 500 });
-  }
-}
+  const data = await db.select().from(products);
 
-// Maneja peticiones POST: http://localhost:3000/api/products
-export async function POST(req: Request) {
-  try {
-    const { name, price, stock, category_id } = await req.json();
-
-    await sql`
-      INSERT INTO products (name, price, stock, category_id)
-      VALUES (${name}, ${price}, ${stock}, ${category_id})
-    `;
-
-    return Response.json({ message: 'Producto creado' }, { status: 201 });
-  } catch (error) {
-    return Response.json({ error: 'Error al crear producto' }, { status: 500 });
-  }
+  return Response.json(data);
 }
